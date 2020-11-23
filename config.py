6c104e658834d41a2088561fb28d2555d7838c54
zero0202 <71034027+zero0202@users.noreply.github.com>
2020-11-23 12:21:50 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,13 +1,12 @@
 import os
 from pathlib import Path
 
-TOKEN = os.getenv("1423900079:AAGW__3wRFANjB8tU87w72Gny4pOl5Q5h58")
-admin_id = int(os.getenv(615399265))
+TOKEN = os.getenv("1423900079:AAHcpFJd5lQprkCV8zdjXUjqFJg2-Hxzjgo")
+admin_id = os.getenv("615399265")
 db_user = os.getenv("anonimchat")
 db_pass = os.getenv("zero+2003")
 # lp_token = os.getenv("LIQPAY_TOKEN")
 host = os.getenv("104.196.100.114")
-
 I18N_DOMAIN = 'anonim_chat_uzbot'
 BASE_DIR = Path(__file__).parent
 LOCALES_DIR = BASE_DIR / 'locales'
